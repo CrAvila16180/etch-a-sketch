14f8f5f465897ec2df3d1d2d07fbafa78a9e2ebc
CR_Avila <carlosrobertoavilah@gmail.com>
2018-12-25 13:00:44 -0600

    .

--- main.js
+++ main.js
@@ -2,13 +2,16 @@ let number = document.querySelector('#number');
 let grid = number.getAttribute('value');
 
 
-
-const createCell = () => {
+const createCell = (length) => {
     const container = document.querySelector('.container')
     const cell = document.createElement('div');
+    const containerSize = container.style.width;
+    let boxSize = `${(Math.floor(100/length))}%`;
     cell.classList.add('cell');
-    container.appendChild(cell)
-
+    cell.style.width = `${boxSize}`;
+    cell.style.height = `${boxSize}`;
+    container.appendChild(cell);
+    
 }
 
 
@@ -16,11 +19,14 @@ function handleUpdate() {
     console.log(this.value)
 }
 
-const generateGrid = (grid) => {
-    for(i = 0; i<grid*2; i++){
-        createCell();
+const generateGrid = (length) => {
+    let value = length;
+    for(i = 0; i<= (value*value); i++){
+        createCell(length);
     }
 }
 
-number.addEventListener('change', generateGrid(grid) );
-number.addEventListener('click', handleUpdate );
+number.addEventListener('change', handleUpdate);
+number.addEventListener('click', handleUpdate);
+
+generateGrid(10);
